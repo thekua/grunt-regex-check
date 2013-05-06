b88e14991ad35dd0116905712c9cf0b9ffecc7ca
Patrick Kua <pkua@thoughtworks.com>
2013-05-06 09:55:17 +0100

    Small cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,8 +15,7 @@ module.exports = function(grunt) {
     jshint: {
       all: [
         'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        'tasks/*.js'
       ],
       options: {
         jshintrc: '.jshintrc',
