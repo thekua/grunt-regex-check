9cc7fc445d49aedb2447aa6213fc7aa80f96f5e1
Patrick Kua <pkua@thoughtworks.com>
2013-05-06 09:52:47 +0100

    Clean up build file

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,11 +37,6 @@ module.exports = function(grunt) {
         }
     },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
     mochaTest: {
           files: ['test/**/*test.js']
     },
@@ -65,7 +60,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'regex-check', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'regex-check', 'mochaTest']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
